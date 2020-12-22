4f1dd2d5126561b589a301699d3ef448dd05cefc
jeki0 <46128027+jeki0@users.noreply.github.com>
2020-12-22 18:08:07 +0300

    Add files via upload

---
+++ bots/bot1.py
@@ -0,0 +1,45 @@
+import asyncio
+import websockets
+import json
+
+
+async def hello():
+    uri = "ws://localhost:8000"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send('{"type":"bot1"}')
+        async for message in websocket:
+            data = json.loads(message)
+            """ Здесь тело бота. Данные полученные с серевера хранятся в объекте data
+            {
+                'point': {
+                    'x': 100,
+                    'y': 100,
+                    'mx': 0.01,
+                    'my': 0.01,
+                },
+                'users': [
+                    {
+                        'x': 0,
+                        'y': 0,
+                        'v': 0,
+                    },
+                    {
+                        'x': 990,
+                        'y': 0,
+                        'v': 0,
+                    }
+                ]
+            }
+            """
+
+            if data['point']['y'] > data['users'][1]['y'] + 5:
+                send_data = 'down'
+
+            if data['point']['y'] < data['users'][1]['y'] + 25:
+                send_data = 'up'
+
+            """ Конец тела бота. для отпрвки вниз или вверх нужну в send_data указать down или up """
+            json_out = json.dumps({'command': send_data, 'from': 1})
+            await websocket.send(json_out)
+
+asyncio.get_event_loop().run_until_complete(hello())
---
+++ server/main.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+# WS server example
+
+import asyncio
+import websockets
+import json
+import threading
+import time
+
+USERS = set()
+game_data = {
+    'point': {
+        'x': 100,
+        'y': 50,
+        'mx': -0.1,
+        'my': 0.1,
+    },
+    'users': [
+        {
+            'x': 0,
+            'y': 0,
+            'v': 0,
+        },
+        {
+            'x': 990,
+            'y': 0,
+            'v': 0,
+        }
+    ]
+}
+match = {}
+bots = 0
+point_speed = 30
+
+
+async def add_user(websocket, first_data):
+    """ Добавление нового пользователя в массив """
+    USERS.add(websocket)
+    # if first_data['type'] == 'bot':
+    #     match[websocket] = 0
+    #     print('bot')
+    # if first_data['type'] == 'bot':
+    #     match[websocket] = 1
+    #     print('bot')
+    if first_data['type'] == 'site':
+        print('site')
+
+
+async def remove_user(websocket):
+    """ Если юзер ливнул то удаляем его """
+    USERS.remove(websocket)
+
+
+def get_data_json():
+    """ Данные пихаем в json и возвращаем их """
+    return json.dumps(game_data)
+
+
+def timer():
+    """ Таймер в отдельном потоке, обновление данных и отправка"""
+    while True:
+        update_point()
+        if USERS:
+            for user in USERS:
+                asyncio.run(user.send(get_data_json()))
+        time.sleep(0.02)
+
+
+def get_first_data(first_data):
+    return json.loads(first_data)
+
+
+def update_point():
+    if game_data['point']['my'] < 0:
+        if game_data['point']['y'] < 0:
+            game_data['point']['my'] = game_data['point']['my'] * -1
+    if game_data['point']['my'] > 0:
+        if game_data['point']['y'] > 790:
+            game_data['point']['my'] = game_data['point']['my'] * -1
+
+    p_y = game_data['point']['y'] + (point_speed* game_data['point']['my'])
+    p_x = game_data['point']['x'] + (point_speed * game_data['point']['mx'])
+
+    for user in game_data['users']:
+        if p_y > user['y'] and p_y < (user['y'] + 30):
+            if p_x > user['x'] and p_x < (user['x'] + 10):
+                game_data['point']['mx'] = game_data['point']['mx'] * -1
+
+    game_data['point']['y'] = game_data['point']['y'] + (point_speed * game_data['point']['my'])
+    game_data['point']['x'] = game_data['point']['x'] + (point_speed * game_data['point']['mx'])
+
+    if game_data['point']['x'] < -10:
+        game_data['point']['x'] = 500
+    if game_data['point']['x'] > 1010:
+        game_data['point']['x'] = 400
+
+    for user in game_data['users']:
+        if user['v'] == 'up':
+            if user['y'] > 0:
+                user['y'] -= 4
+        if user['v'] == 'down':
+            if user['y'] < 790:
+                user['y'] += 4
+
+
+def update_user(data, websocket):
+    game_data['users'][data['from']]['v'] = data['command']
+
+
+async def connect(websocket, path):
+    """ Работаем с новым подключением """
+    print('Connected')
+    first_data_json = await websocket.recv()
+    first_data = get_first_data(first_data_json)
+    await add_user(websocket, first_data)
+    try:
+        await websocket.send(get_data_json())
+        async for message in websocket:
+            data = json.loads(message)
+            update_user(data, websocket)
+    finally:
+        await remove_user(websocket)
+
+start_server = websockets.serve(connect, "localhost", 8080)
+
+x = threading.Thread(target=timer, args=())
+x.start()
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
